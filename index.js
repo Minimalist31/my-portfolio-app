df6015ea62050d78990ca99b7366168dbee28719
Minimalist31 <96783107+Minimalist31@users.noreply.github.com>
2024-06-04 07:45:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const viewportMetaTag = document.createElement("meta");
+viewportMetaTag.name = "viewport";
+viewportMetaTag.content = "width=device-width, initial-scale=1";
+document.head.appendChild(viewportMetaTag);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
